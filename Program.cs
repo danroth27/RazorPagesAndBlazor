36b05bdc9e6901f9028f76a58f288dad2f3aae9d
Daniel Roth <daroth@microsoft.com>
2023-09-28 17:21:28 -0700

    Add Blazor SSR

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
+using RazorPagesAndBlazor.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddRazorComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -20,6 +23,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseAntiforgery();
+
 app.MapRazorPages();
+app.MapRazorComponents<App>();
 
 app.Run();
